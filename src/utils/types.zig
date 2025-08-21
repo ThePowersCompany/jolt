3a0110a31f91ebfa2be5f5b7c493bcf0b7e5cd41
Richard Dunlap <rdunlap@thepowerscompany.com>
2025-08-21 13:29:07 -0400

    Improved Optional type.

--- src/utils/types.zig
+++ src/utils/types.zig
@@ -2,18 +2,33 @@
 const std = @import("std");
 const json = std.json;
 
+pub fn Unwrap(T: type) type {
+    const info = @typeInfo(T);
+    if (info == .optional) return Unwrap(info.optional.child);
+    return T;
+}
+
+pub fn unwrap(T: type, t: T) ?Unwrap(T) {
+    const info = @typeInfo(T);
+    if (info == .optional) {
+        if (t) |inner| {
+            return unwrap(info.optional.child, inner);
+        }
+    }
+    return t;
+}
+
 pub fn Optional(comptime T: type) type {
     return union(enum) {
         not_provided,
-        value: ?T,
+        value: T,
 
         const Self = @This();
 
         /// Returns the value if it is present in this Optional, otherwise returns null.
-        pub fn get(self: Self) ?T {
-            if (self == .value) {
-                return self.value;
-            }
+        /// This function will unwrap multiple levels of null, down to the actual value.
+        pub fn get(self: Self) ?Unwrap(T) {
+            if (self == .value) return unwrap(T, self.value);
             return null;
         }
 
@@ -41,9 +56,9 @@ pub fn Optional(comptime T: type) type {
 
 test "Optional" {
     const Foo = struct {
-        foo: Optional(i32) = .not_provided,
-        bar: Optional(i32) = .not_provided,
-        baz: Optional(i32) = .not_provided,
+        foo: Optional(?i32) = .not_provided,
+        bar: Optional(?i32) = .not_provided,
+        baz: Optional(?i32) = .not_provided,
     };
 
     const payload =
@@ -59,6 +74,20 @@ test "Optional" {
     defer parsed.deinit();
 
     const foo = parsed.value;
+
+    {
+        const inner = foo.foo.get();
+        if (inner) |v| {
+            try std.testing.expect(v == 123);
+        } else try std.testing.expect(false);
+    }
+
+    {
+        const inner = foo.bar.get();
+        if (inner) |_| {
+            try std.testing.expect(false);
+        } else try std.testing.expect(inner == null);
+    }
 
     if (foo.foo.get()) |v| {
         try std.testing.expect(v == 123);
