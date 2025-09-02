84ab53835f2d0d3dc4186a08f97e7e2bbe7d5264
Richard Dunlap <rdunlap@thepowerscompany.com>
2025-09-02 16:09:08 -0400

    Fixed and wrote tests from json stringify.

--- src/utils/json.zig
+++ src/utils/json.zig
@@ -10,7 +10,30 @@ pub fn stringify(alloc: Allocator, value: anytype, options: std.json.Stringify.O
 }
 
 /// Caller owns the returned memory.
-pub fn stringifyBuf(buffer: []u8, value: anytype, options: std.json.Stringify.Options) !void {
+pub fn stringifyBuf(buffer: []u8, value: anytype, options: std.json.Stringify.Options) !usize {
     var writer = std.Io.Writer.fixed(buffer);
     try std.json.Stringify.value(value, options, &writer);
+    return writer.buffered().len;
+}
+
+test "stringify" {
+    const alloc = std.testing.allocator;
+    const pong_json = stringify(alloc, .{ "pong", 123 }, .{}) catch |err| {
+        std.log.err("{}", .{err});
+        return try std.testing.expect(false);
+    };
+    defer alloc.free(pong_json);
+
+    try std.testing.expectEqualStrings("[\"pong\",123]", pong_json);
+}
+
+test "stringifyBuf" {
+    var buffer: [256]u8 = undefined;
+    const written_bytes = stringifyBuf(&buffer, .{ "pong", 123 }, .{}) catch |err| {
+        std.log.err("{}", .{err});
+        return try std.testing.expect(false);
+    };
+
+    const pong_json = buffer[0..written_bytes];
+    try std.testing.expectEqualStrings("[\"pong\",123]", pong_json);
 }
