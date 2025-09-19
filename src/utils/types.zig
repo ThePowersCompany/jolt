167f37e34f955e5bb75d6198d134e432162218c9
Bradley Odell <btodell@hotmail.com>
2025-09-19 11:41:22 -0800

    json array

--- src/utils/types.zig
+++ src/utils/types.zig
@@ -99,3 +99,76 @@ test "Optional" {
 
     if (foo.bar.value) |_| try std.testing.expect(false);
 }
+
+pub fn JsonArray(comptime T: type) type {
+    return struct {
+        const Self = @This();
+
+        list: std.ArrayList(T) = .empty,
+
+        pub fn jsonParse(
+            allocator: std.mem.Allocator,
+            source: anytype,
+            options: json.ParseOptions,
+        ) json.ParseError(@TypeOf(source.*))!Self {
+            const slice: []T = try json.innerParse([]T, allocator, source, options);
+            return .{ .list = .fromOwnedSlice(slice) };
+        }
+
+        pub fn jsonParseFromValue(
+            allocator: std.mem.Allocator,
+            source: json.Value,
+            options: json.ParseOptions,
+        ) json.ParseError(@TypeOf(allocator))!Self {
+            const slice: []T = try json.parseFromValueLeaky(T, allocator, source, options);
+            return .{ .list = .fromOwnedSlice(slice) };
+        }
+
+        pub fn jsonStringify(self: *const Self, jws: anytype) !void {
+            try json.Stringify.write(jws, self.list.items);
+        }
+    };
+}
+
+test "parse json array" {
+    const alloc = std.testing.allocator;
+
+    const Foo = struct {
+        data: JsonArray(i32),
+    };
+
+    const payload =
+        \\ { "data": [1, 2, 3] }
+    ;
+
+    const parsed = try json.parseFromSlice(Foo, alloc, payload, .{});
+    defer parsed.deinit();
+
+    const foo = parsed.value;
+
+    try std.testing.expect(foo.data.list.items[0] == 1);
+    try std.testing.expect(foo.data.list.items[1] == 2);
+    try std.testing.expect(foo.data.list.items[2] == 3);
+}
+
+test "stringify json array" {
+    const stringify = @import("json.zig").stringify;
+    const alloc = std.testing.allocator;
+
+    const Foo = struct {
+        data: JsonArray(i32),
+    };
+
+    var foo: Foo = .{
+        .data = .{},
+    };
+    defer foo.data.list.deinit(alloc);
+    try foo.data.list.append(alloc, 1);
+    try foo.data.list.append(alloc, 2);
+    try foo.data.list.append(alloc, 3);
+
+    const str = try stringify(alloc, foo, .{});
+    defer alloc.free(str);
+
+    try std.testing.expectEqualStrings(str, "{\"data\":[1,2,3]}");
+}
